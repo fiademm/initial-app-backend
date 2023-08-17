cc2e0f60bc7a54a89011b31642758c4d484109f4
fiademm <fiademm1@gmail.com>
2023-08-17 14:37:19 +0000

    Update endpoints

--- server.js
+++ server.js
@@ -75,14 +75,65 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+// Instructor login
+app.post('/instructor/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const result = await pool.query(
+        'SELECT * FROM "instructor_details" WHERE email = $1',
+        [email]
+      );
+  
+      const user = result.rows[0];
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+  
+      const token = jwt.sign({ id: user.id }, jwtSecret, { expiresIn: '1h' });
+  
+      res.json({ token });
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+
+// Instructor registration
+app.post('/instructor/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+  
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      await pool.query(
+        'INSERT INTO "instructor_details" (name, email, password) VALUES ($1, $2, $3)',
+        [name, email, hashedPassword]
+      );
+  
+      res.json({ message: 'Registration successful' });
+    } catch (error) {
+      console.error('Error during signup:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+
 // Learner enrolls in a course
 app.post('/enroll', async (req, res) => {
-  const { userId, courseId } = req.body;
+  const { learner_id, course_id } = req.body;
+
+  const enrollment_date = new Date();
 
   try {
-    await pool.query('INSERT INTO Enrollment (User_ID, Course_ID) VALUES ($1, $2)', [
-      userId,
-      courseId,
+    await pool.query('INSERT INTO course_enrollment (learner_id, course_id, enrollment_date) VALUES ($1, $2, $3)', [
+        learner_id, course_id, enrollment_date
     ]);
 
     res.json({ message: 'Enrollment successful' });
@@ -95,7 +146,7 @@ app.post('/enroll', async (req, res) => {
 // Get all courses
 app.get('/courses', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM Course');
+    const result = await pool.query('SELECT * FROM courses');
     const courses = result.rows;
 
     res.json(courses);
@@ -105,13 +156,66 @@ app.get('/courses', async (req, res) => {
   }
 });
 
-// Get all learners
+// Get all course enrollments
+app.get('/courses/enrollments', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM course_enrollment');
+      const courses = result.rows;
+  
+      res.json(courses);
+    } catch (error) {
+      console.error('Error retrieving courses:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+
+// Get all enrolled courses for a particular user
+app.get('/courses/enrollments/:learner_id', async (req, res) => {
+    try {
+    const learner_id = req.params.learner_id;
+    const result = await pool.query('SELECT * FROM course_enrollment WHERE learner_id = $1', [learner_id]);
+    const courses = result.rows;
+    
+      res.json(courses);
+    } catch (error) {
+      console.error('Error retrieving courses:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+    });
+
+// Get all learners (users)
 app.get('/learners', async (req, res) => {
     try {
       const result = await pool.query('SELECT * FROM learner_details');
       const learners = result.rows;
   
       res.json(learners);
+    } catch (error) {
+      console.error('Error retrieving users:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+
+// Get all instructors (users)
+app.get('/instructors', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM instructor_details');
+      const instructors = result.rows;
+  
+      res.json(instructors);
+    } catch (error) {
+      console.error('Error retrieving users:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+
+// Get all admins (users)
+app.get('/admins', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM administrator_details');
+      const administrators = result.rows;
+  
+      res.json(administrators);
     } catch (error) {
       console.error('Error retrieving users:', error);
       res.status(500).json({ message: 'Server error' });
@@ -153,12 +257,11 @@ app.post('/admin/login', async (req, res) => {
 
 // Admin adds a new course
 app.post('/admin/courses', async (req, res) => {
-  const { title, description } = req.body;
+  const { title, description, objectives, level, duration, instructor_id, prerequisites, certification, language, tag } = req.body;
 
   try {
-    await pool.query('INSERT INTO Course (Title, Description) VALUES ($1, $2)', [
-      title,
-      description,
+    await pool.query('INSERT INTO courses (title, description, objectives, level, duration, instructor_id, prerequisites, certification, language, tag) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [
+      title, description, objectives, level, duration, instructor_id, prerequisites, certification, language, tag
     ]);
 
     res.json({ message: 'Course added successfully' });
